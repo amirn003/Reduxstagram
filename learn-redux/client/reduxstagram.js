f52d5253cbcad8bc0fd0f67f268f8c7c504764fb
amirn003 <amir.naar@gmail.com>
2023-12-13 10:58:05 +0100

    import components to reduxstagram

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,6 +2,27 @@ import React from 'react';
 import { render } from 'react-dom';
 
 // Import css
-import css from './styles/style.styl'
+import css from './styles/style.styl';
 
-render(<p>Hi</p>, document.getElementById('root'));
+// Import Components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
+
+
+// render(<Main/>, document.getElementById('root'));
+// render(<Main><p>Hello</p></Main> document.getElementById('root'));
