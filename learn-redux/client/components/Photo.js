27a9f45126c0d1bd604eb1a7ee9c20e560fb1ef1
amirn003 <amir.naar@gmail.com>
2023-12-15 12:04:35 +0100

    add photogrid context and set photo components

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -1,10 +1,16 @@
 import React from 'React';
+import { Link } from 'react-router';
 
 const Photo = React.createClass({
   render() {
     return (
       <figure className='grid-figure'>
         I'm a photo!
+        <div className='grid-photo-wrap'>
+          <Link to={`/view/${this.props.post.code}`}>
+            {this.props.post.caption}
+          </Link>
+        </div>
       </figure>
     )
   }
