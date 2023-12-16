1d4bc3a68addbdb3ee4c600a477b5e5ff26d1c2a
amirn003 <amir.naar@gmail.com>
2023-12-16 15:13:58 +0100

    starting adding comments

--- learn-redux/client/components/Comment.js
+++ learn-redux/client/components/Comment.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Comments = React.createClass({
+  renderComment(comment, i){
+    console.log(comment);
+  },
   render() {
     return (
       <div className='comment'>
         I'm the comments
+        {/* {this.props.postComments.map(this.renderComment)} */}
       </div>
     )
 
