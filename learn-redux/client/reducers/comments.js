0f51cf12e6d9ae03c936b22feb2bf3da6929a8b9
amirn003 <amir.naar@gmail.com>
2023-12-17 09:58:54 +0100

    add post comments

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,5 +1,25 @@
+function postComments(state = [], action) {
+  switch(action.type){
+    case 'ADD_COMMENT':
+    case 'REMOVE_COMMENT':
+      return state;
+    default:
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
   console.log(state, action);
+  if(typeof action.postId !== 'undefined'){
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+
+    }
+  }
   return state;
 }
 
