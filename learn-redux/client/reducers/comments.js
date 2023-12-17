a0725b737f6fbc9bfbd2bfbb05a5061b3207d794
amirn003 <amir.naar@gmail.com>
2023-12-17 10:01:49 +0100

    adding new comments

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,6 +1,11 @@
 function postComments(state = [], action) {
   switch(action.type){
     case 'ADD_COMMENT':
+      // return the new state with the new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
     case 'REMOVE_COMMENT':
       return state;
     default:
