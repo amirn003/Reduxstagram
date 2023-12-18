1cda02ba74db77719cc2cc7fba7e0166d0dec1a4
amirn003 <amir.naar@gmail.com>
2023-12-18 12:53:34 +0100

    hot reloading redux reducers with webpack

--- client/store.js
+++ client/store.js
@@ -8,7 +8,6 @@ import posts from './data/posts';
 
 
 // create an object for the default data
-
 const defaultStore = {
   posts,
   comments
@@ -17,5 +16,12 @@ const defaultStore = {
 const store = createStore(rootReducer, defaultStore);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+if (module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  })
+}
 
 export default store;
